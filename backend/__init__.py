caef1fea9f7362c2a50d8355903bec9322070e52
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-04-24 18:58:31 +0200

    added configuration file option

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,15 @@
+__author__ = 'Tristan Trouwen'
+
+from os import environ
 from flask import Flask
 
 app = Flask(__name__)
+
+if environ.get('FLASK_ENV').lower() == 'production':
+    app.config.from_object('backend_configuration.ProductionConfig')
+
+else:  # default to development configuration
+    app.config.from_object('backend_configuration.DevelopmentConfig')
+
 
 import backend.views
