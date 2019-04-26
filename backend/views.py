a69986fad00203771aef75074dc25cd4a19b4c80
Johannes Kool <j.kool@student.tue.nl>
2019-04-26 12:58:39 +0200

    disabled caching for now

--- backend/views.py
+++ backend/views.py
@@ -36,6 +36,17 @@ def hello_world():
             flash("Successfully uploaded!")
             return render_template("index.html")
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 class Data(Resource):
     def get(self, data_name):
