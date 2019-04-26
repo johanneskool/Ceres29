dfa891c65a95b567a4e53e6708dc14b2fcaade8f
JustSamuel <samuel.oosterholt20001004@gmail.com>
2019-04-26 12:27:56 +0200

    Main.js no generates a random matrix

--- backend/static/scripts/main.js
+++ backend/static/scripts/main.js
@@ -1,7 +1,79 @@
-function setup() {
+var NODE_COUNT = 100;
+var node_width = 500 / NODE_COUNT;
+var nodes = [];
+var matrix;
 
+function setup() {
+    colorMode(HSL,100);
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    var matrixSize = NODE_COUNT * node_width;
+    matrix = createGraphics(matrixSize, matrixSize);
+    matrix.colorMode(HSL,100);
+
+    frameRate(999);
+
+    noStroke();
+    matrix.noStroke();
+
+    generate_matrix();
+    draw_matrix();
+
+}
+
+function generate_matrix () {
+    for (var i = 0; i < NODE_COUNT; i++) {
+        nodes.push(new Node());
+        for (var j = 0; j < NODE_COUNT; j++) {
+            if (random(1) < 0.5) {
+                nodes[i].outgoing.push(0);
+            } else {
+                nodes[i].outgoing.push(random(0,1));
+            }
+        }
+    }
+}
+
+function draw_matrix() {
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].drawOutgoing();
+        matrix.translate(node_width,0);
+    }
 }
 
 function draw() {
+    background(0,0,0);
+    fill(0, 0, 0, 100);
 
+    push();
+    var width = window.innerWidth / 2 - (NODE_COUNT * node_width) / 2;
+    var height = window.innerHeight / 2 - (NODE_COUNT * node_width) / 2;
+    translate(width,height);
+    image(matrix,0,0);
+    pop();
 }
+
+function Node() {
+    this.outgoing = [];
+}
+
+Node.prototype.drawOutgoing = function () {
+    matrix.push();
+    for (var i = 0; i < this.outgoing.length; i++) {
+        var hue = map(this.outgoing[i],0,1,25,0);
+        var opacity = map(this.outgoing[i],0,1,25,100);
+        matrix.fill(hue,75,50,opacity);
+        matrix.rect(0,0,node_width,node_width);
+        matrix.translate(0,node_width);
+    }
+    matrix.pop();
+};
+
+/**
+ * Rescales the canvas when the windows has been changed
+ */
+window.onresize = function() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    resizeCanvas(w, h);
+};
