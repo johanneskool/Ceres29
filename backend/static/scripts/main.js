4c74feaae98917a0f94e9d9c393fd4d2afe95e87
Samuel <samuel.oosterholt20001004@gmail.com>
2019-04-26 16:50:14 +0200

    Added (very) basic zooming functionality

--- backend/static/scripts/main.js
+++ backend/static/scripts/main.js
@@ -1,43 +1,43 @@
-var NODE_COUNT = 1000;
-var node_width = 500 / NODE_COUNT;
+var NODE_COUNT = 500;
+var NODE_SIZE = 10;
 var nodes = [];
 var matrix;
 
-var xOff = 0;
-var yOff = 0;
-var oldMouseX = 0;
-var oldMouseY = 0;
-var newMouseX = 0;
-var newMouseY = 0;
-
 var matrixSize;
-var canvas;
+var matrixWidth;
+var matrixHeight; //matrix is square so no real reason to differ between height and width
+var defaultCanvas;
+var body;
+var zoomScale = 1;
 
 //basic setup and buffer for matrix to prevent redrawing.
 function setup() {
+
     colorMode(HSL,100);
     createCanvas(window.innerWidth, window.innerHeight);
+    //canvas.mouseWheel(zoom);
 
-    node_width = ceil(min(window.innerWidth, window.innerHeight) / (1.5*NODE_COUNT));
-
-    canvas = document.getElementsByTagName('canvas')[0];
     //make matrix buffer graphics
-    matrixSize = NODE_COUNT * node_width;
+    matrixSize = NODE_COUNT * NODE_SIZE;
     matrix = createGraphics(matrixSize, matrixSize);
     matrix.colorMode(HSL,100);
 
     frameRate(999);
 
+    matrixWidth = ceil(min(windowHeight, windowWidth) / 1.5);
+
+    imageMode(CENTER);
+    matrix.imageMode(CENTER);
+    rectMode(CENTER);
     noStroke();
     matrix.noStroke();
 
-    generate_matrix();
-    draw_matrix();
-
+    generateMatrix();
+    drawMatrix();
 }
 
 //function that generates random matrix values.
-function generate_matrix () {
+function generateMatrix () {
     for (var i = 0; i < NODE_COUNT; i++) {
         nodes.push(new Node());
         for (var j = 0; j < NODE_COUNT; j++) {
@@ -51,12 +51,19 @@ function generate_matrix () {
 }
 
 //invoke function for drawing the matrix to the buffer
-function draw_matrix() {
+function drawMatrix() {
     for (var i = 0; i < nodes.length; i++) {
         nodes[i].drawOutgoing();
-        matrix.translate(node_width,0);
+        matrix.translate(NODE_SIZE,0);
     }
 }
+
+var xOff = 0;
+var yOff = 0;
+var oldMouseX = 0;
+var oldMouseY = 0;
+var newMouseX = 0;
+var newMouseY = 0;
 
 function mousePressed() {
     print('clicked');
@@ -82,12 +89,23 @@ function draw() {
     background(0,0,0);
     fill(0, 0, 0, 100);
 
-    push();
-    var width = window.innerWidth / 2 - (NODE_COUNT * node_width) / 2;
-    var height = window.innerHeight / 2 - (NODE_COUNT * node_width) / 2;
-    translate(width + xOff,height + yOff);
-    image(matrix,0,0,300,300);
-    pop();
+    //push();
+    resetMatrix();
+    var matrixX = windowWidth / 2 + xOff;
+    var matrixY = windowHeight / 2 + yOff;
+    image(matrix,matrixX,matrixY,matrixWidth/zoomScale,matrixWidth/zoomScale);
+    //pop();
+}
+
+//TODO make zooming go to center of screen.
+function mouseWheel () {
+    print(event.deltaY);
+    if (event.deltaY > 0) {
+        zoomScale = zoomScale / 2;
+    } else {
+        zoomScale = zoomScale * 2;
+    }
+    print(zoomScale);
 }
 
 /**
@@ -106,8 +124,8 @@ Node.prototype.drawOutgoing = function () {
         var hue = map(this.outgoing[i],0,1,25,0);
         var opacity = map(this.outgoing[i],0,1,25,100);
         matrix.fill(hue,75,50,opacity);
-        matrix.rect(0,0,node_width,node_width);
-        matrix.translate(0,node_width);
+        matrix.rect(0,0,NODE_SIZE,NODE_SIZE);
+        matrix.translate(0,NODE_SIZE);
     }
     matrix.pop();
 };
