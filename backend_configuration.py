2623b1020ff9e7353a13268902705d932817c9b0
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-04-29 15:29:02 +0200

    removed debug info

--- backend_configuration.py
+++ backend_configuration.py
@@ -17,7 +17,6 @@ class Config(object):
     UPLOAD_FOLDER = os.path.join(basedir, 'backend/static/uploads/')
     JSON_FOLDER_RELATIVE = 'json'
     JSON_FOLDER = os.path.join(basedir, 'backend/static/', JSON_FOLDER_RELATIVE)
-    print(JSON_FOLDER)
     ALLOWED_EXTENSIONS = set(['csv'])
     SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
