fcf3840b6f505f5b7a31674a4b9e9896c2ffa8e4
AkBoy7 <akambilbas@gmail.com>
2019-05-01 15:37:48 +0200

    testSigma.js cleaned up code and now supports up to 1256/2512 nodes

--- backend/static/scripts/sigma_test.js
+++ backend/static/scripts/sigma_test.js
@@ -2,10 +2,17 @@ var sizeCircle = (window.innerHeight - 125);
 var xValues = [];
 var yValues = [];
 var Nodes = [];
+//Changing the amount of nodes:
+//factorCircle should be:
+// 32 -> 2512 nodes
+// 16 -> 1256 nodes
+// 8  -> 628 nodes
+//etc...
 var inputSize = 1256;
+var factorCircle = 16;
 
 function setup() {
-    createCircleArea();
+    createCircleArea(); //creates coordinates for circle
     var s = new sigma(
         {
             renderer: {
@@ -17,30 +24,32 @@ function setup() {
                 maxEdgeSize: 4,
                 minNodeSize: 1,
                 maxNodeSize: 5,
-                minArrowSize: 10
+                minArrowSize: 10,
+                defaultLabelAlignment: 'left'
             }
         }
     );
 
 // Generate a random graph:
     var nbNode = inputSize;
-    var nbEdge = floor(inputSize/2);
+    var nbEdge = floor(inputSize/3);
     var graph = {
         nodes: [],
         edges: []
     };
 
-    for (var i = 0; i < nbNode; i++)
+    for (var i = 0; i < nbNode; i++) {
         graph.nodes.push({
-            id:  i,
+            id: i,
             label: 'Node ' + i,
             x: xValues[i],
             y: yValues[i],
             size: 1,
             color: '#EE651D',
         });
+    }
 
-    for (var i = 0; i < nbEdge; i++)
+    for (var i = 0; i < nbEdge; i++) {
         graph.edges.push({
             id: i,
             source: '' + (Math.random() * nbNode | 0),
@@ -48,6 +57,7 @@ function setup() {
             color: '#202020',
             type: 'curvedArrow',
         });
+    }
 
     // Load the graph in sigma
     s.graph.read(graph);
@@ -59,7 +69,7 @@ function createCircleArea() {
     var centerX = width/2;
     var centerY = height/2;
     var radius = sizeCircle/2;
-    var steps = 16 * 3.14;
+    var steps = factorCircle * 3.14;
     for (var i = 0; i < inputSize; i++) {
         var phase = 2 * Math.PI * i / steps;
         xValues[i] = (centerX + radius * Math.cos(phase));
