3c33e6457ef5a3b37ad4f9f5766bfc4a1875be8f
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-05-06 12:18:24 +0200

    bug fixes

--- backend/static/test/sketch.js
+++ backend/static/test/sketch.js
@@ -1,8 +1,3 @@
-let data = {"henry": [1,2,3,0,0],
-            "jan": [2,5,9,0,0],
-            "dave": [0,0,0,0,2],
-            "bob": [1,0,0,0,3],
-            "ellen": [2,2,0,0,0]};
 
 let nodes = [];
 let radius = 0;
@@ -11,8 +6,7 @@ let circleX = 0;
 let circleY = 0;
 
 let currentActive = null;
-
-let limit = 100;
+let limit = 100; // at most 100 nodes can be put on the circle
 
 function preload() {
   // get json data
@@ -34,15 +28,15 @@ function setup() {
     let new_node = new Node(
       entry,
       number,
-      number*2*Math.PI/Math.min(Object.keys(data).length, limit),
+      number*2*Math.PI/(Math.min(Object.keys(data).length, limit)+1),
       radius,
       data[entry]
     );
         
-    nodes.push(new_node);
+    nodes.push(new_node); // put in array
     number++;
     if (number > limit) {
-      break;
+      break; // stop adding nodes if the limit of nodes is reached
     }
   }
   
@@ -72,19 +66,24 @@ function setup() {
 function draw() {
     background(141,141,141);
     noFill();
-    circle(circleX, circleY,2*radius);
+    // circle(circleX, circleY,2*radius); // draws big circle around which all nodes will be put
+
+    // draw each node
     nodes.forEach(node => {
       node.draw_node();
-      if (currentActive.angle > 0.05) {
-        // rotate all nodes
-        node.angle = (node.angle + 0.05) % (Math.PI*2);
-      }
     });
 
+    // rotate all nodes if needed
+    if (currentActive.angle > 0.1) {
+      nodes.forEach(node => {
+        node.angle = (node.angle + 0.1) % (Math.PI * 2);
+      })
+    }
 }
 
 
 function mouseClicked() {
+
     nodes.forEach(node => {
         if (dist(mouseX, mouseY, node.location_x(), node.location_y()) < node.radius) {
           currentActive.active = false;
@@ -93,7 +92,6 @@ function mouseClicked() {
         }
     });
 }
-
 
 
 function Node(name, number, angle, outside_radius) {
@@ -116,9 +114,11 @@ function Node(name, number, angle, outside_radius) {
   this.edges = [];
   
   this.draw_node = function() {
+    stroke(0,0,0);
     circle(this.location_x(), this.location_y(), this.radius);
     // draw label
     push();
+    // black color
     stroke(0,0,0);
     this.outside_radius += this.radius; // hack to make the text go outward a bit, revert after drawing text
     translate(this.location_x(), this.location_y());
@@ -126,20 +126,24 @@ function Node(name, number, angle, outside_radius) {
     text(this.name, 0,0);
     this.outside_radius -= this.radius;
     pop();
-
+    // only draw edges of ac
     if (this.active) {
       this.drawEdges();
     }
   };
+
   this.drawEdges = function() {  
     // draw edges
     for (let i = 0; i < this.edges.length; i++) {
       try {
         let to_node = this.edges[i].to_node;
         noFill();
+        push();
+        stroke(this.edges[i].weight*10,this.edges[i].weight*10,this.edges[i].weight*10);
         bezier(this.location_x(), this.location_y(), circleX, circleY, to_node.location_x(), to_node.location_y(), to_node.location_x(), to_node.location_y());
+        pop();
       } catch(e) {
-        console.log(e);
+        // to_node is not defined (the case when to_node was not drawn due to lack of space)
       }
     }
   }
@@ -149,5 +153,5 @@ function Node(name, number, angle, outside_radius) {
 
 function DirectedEdge(to, weight) {
   this.to_node = to;
-  this.weigth = weight;
+  this.weight = weight;
 }
