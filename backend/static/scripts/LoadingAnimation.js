564efac66ff05f0e46d7aee2592423b34593932c
AkBoy7 <akambilbas@gmail.com>
2019-05-06 13:39:49 +0200

    Merge branch 'master' of C:\Users\20172523\IdeaProjects\Ceres29 with conflicts.

--- backend/static/scripts/LoadingAnimation.js
+++ backend/static/scripts/LoadingAnimation.js
@@ -1,68 +1,52 @@
 //Author: Fabiénne
-var sketch = function (animation) {
-  animation.centerX = window.innerWidth/2;
-  animation.centerY = window.innerHeight/2;
-  animation.radius = 100;
-  animation.circleArray = [];
 
-  animation.onresize = function() {
-    animation.w = window.innerWidth;
-    animation.h = window.innerHeight;
-    animation.resizeCanvas(animation.w, animation.h);
-    animation.centerX = window.innerWidth/2;
-    animation.centerY = window.innerHeight/2;
-  };
+const centerX = window.innerWidth/2;
+const centerY = window.innerHeight/2;
+const radius = 100;
+let circleArray = [];
 
-  animation.setup = function () {
-    animation.animationCanvas = animation.createCanvas(window.innerWidth, window.innerHeight);
-    animation.animationCanvas.parent('canvas');
-    document.getElementById("defaultCanvas1").style.zIndex = 2;
-    animation.noStroke();
-    for (var i = 0; i < 7; i++) {
-      animation.circleArray.push(new animation.Circle(-80 + 20*i, 0, 120 + i*20, 100 + i*25));
-    }
-  };
-
-  animation.draw = function () {
-    animation.clear();
-    if(!visIsLoaded) {
-      for (var i = 0; i < animation.circleArray.length; i++) {
-        animation.circleArray[i].move();
-        animation.circleArray[i].show();
-      }
-    }
-  };
-
-  animation.Circle = function (angle, r, g, b) {
-    this.angle = animation.radians(angle);
+class Circle {
+  constructor(angle, r, g, b) {
+    this.angle = radians(angle);
     this.r = r;
     this.g = g;
     this.b = b;
     this.speed = 0.02;
-    this.x = animation.centerX + animation.radius;
-    this.y = animation.centerY + animation.radius;
-    this.move = function () {
-      this.x = animation.centerX + animation.radius * animation.cos(this.angle);
-      this.y = animation.centerY + animation.radius * animation.sin(this.angle);
-    };
-    this.show = function () {
-      animation.fill(this.r, this.g, this.b);
-      animation.ellipse(this.x, this.y, 25,25);
-      this.angle = this.angle + this.speed;
-
-
-      if (this.x > animation.centerX) {
-        this.speed = 0.07;
-      } else {
-        this.speed = 0.04;
-      }
-    };
+    this.x = centerX + radius;
+    this.y = centerY + radius;
   }
-};
 
-var loadingAnimation = new p5(sketch);
+  move() {
+    this.x = centerX + radius * cos(this.angle);
+	this.y = centerY + radius * sin(this.angle);
+  }
 
-window.onresize = function() {
-  loadingAnimation.onresize();
-  console.log("resize");
-};
+  show() {
+    fill(this.r, this.g, this.b);
+    ellipse(this.x, this.y, 25);
+    this.angle = this.angle + this.speed;
+
+
+    if (this.x > centerX) {
+      this.speed = 0.07;
+        } else {
+      this.speed = 0.04;
+        }
+  }
+}
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noStroke();
+  for (let i = 0; i < 7; i++) {
+    circleArray.push(new Circle(-80 + 20*i, 0, 120 + i*20, 100 + i*25));
+  }
+}
+
+function draw() {
+  background(255);
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].move();
+    circleArray[i].show();
+  }
+}
