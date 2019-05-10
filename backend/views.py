d33162267a0d28e77f969218bd8adf63d1fecc2e
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-05-10 11:21:36 +0200

    created json endpoints and updated templates and front end accordinly

--- backend/views.py
+++ backend/views.py
@@ -2,10 +2,10 @@ __author__ = 'Tristan Trouwen, Johannes Kool, Rick Luiken, Rink Pieters'
 
 import os
 
-from flask import render_template, request, redirect, flash, url_for
+from flask import render_template, request, redirect, flash, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-from backend import app
+from backend import app, db
 from backend.orm.models import File
 
 
@@ -14,98 +14,74 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
+def get_available_files():
+    return File.query.all()
+
+
+def handle_file_upload(request_upload):
+    # check if the post request has the file part
+    if 'file' not in request_upload.files:
+        flash('No file part')
+        return redirect(request_upload.url)
+    file = request_upload.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request_upload.url)
+    if not allowed_file(file.filename):
+        flash('Filetype not allowed')
+        return redirect(request_upload.url)
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # process file into different forms and track this in db
+        new_file = File(filename, name=filename.split('.csv')[0])
+        db.session.add(new_file)
+        db.session.commit()
+
+        flash("Successfully uploaded!")
+        return redirect(url_for('index'))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    data_name = request.args.get('data')
+    data_id = request.args.get('data')
     if request.method == 'GET':
-        # find available JSON files
-        json_file_path = app.config['JSON_FOLDER']
-        available_files = [{
-            "url": url_for('static', filename=os.path.join(app.config['JSON_FOLDER_RELATIVE'], file)),
-            "filename": file
-        }
-            for file in os.listdir(json_file_path) if
-            os.path.isfile(os.path.join(json_file_path, file)) and file != 'readme.md'
-        ]
-
-        return render_template("index.html", files_available=available_files, app=app, data=data_name, title=data_name)
+        return render_template("index.html", files_available=get_available_files(), data=data_id, title="Home")
 
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if not allowed_file(file.filename):
-            flash('Filetype not allowed')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            File(filepath)
-            flash("Successfully uploaded!")
-            return redirect(url_for('index'))
+        return handle_file_upload(request)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    data_name = request.args.get('data')
+    data_id = request.args.get('data')
     if request.method == 'GET':
-        # find available JSON files
-        json_file_path = app.config['JSON_FOLDER']
-        available_files = [{
-            "url": url_for('static', filename=os.path.join(app.config['JSON_FOLDER_RELATIVE'], file)),
-            "filename": file
-        }
-            for file in os.listdir(json_file_path) if
-            os.path.isfile(os.path.join(json_file_path, file)) and file != 'readme.md'
-        ]
-
-        return render_template("upload.html", files_available=available_files, app=app, data=data_name, title="Upload a file")
+        return render_template("upload.html", files_available=get_available_files(), data=data_id, title="Upload a file")
 
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if not allowed_file(file.filename):
-            flash('Filetype not allowed')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            File(filename, name=os.path.splitext(filename)[0])
-            flash("Successfully uploaded!")
-            return redirect(url_for('index'))
+        return handle_file_upload(request)
 
 
 @app.route('/vis', methods=['GET'])
 def vis():
-    data_name = request.args.get('data')
+    data_id = request.args.get('data')
+    data_name = File.query.get(data_id).name
     if request.method == 'GET':
-        # find available JSON files
-        json_file_path = app.config['JSON_FOLDER']
-        available_files = [{
-            "url": url_for('static', filename=os.path.join(app.config['JSON_FOLDER_RELATIVE'], file)),
-            "filename": file
-        }
-            for file in os.listdir(json_file_path) if
-            os.path.isfile(os.path.join(json_file_path, file)) and file != 'readme.md'
-        ]
+        return render_template("vis.html", files_available=get_available_files(), data=data_name, title=data_name)
 
-        return render_template("vis.html", files_available=available_files, app=app, data=data_name, title=data_name)
+
+@app.route('/data/<int:id>')
+def data(id):
+    type = request.args.get('type')
+    file = File.query.get(id)
+
+    if type == 'fiedler':
+        return send_from_directory('static', file.fiedler)
+    else:
+        return send_from_directory('static', file.default)
 
 
 @app.after_request
