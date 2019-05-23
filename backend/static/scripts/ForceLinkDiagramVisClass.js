7ccd4aae14634328fc7a4453ed6af0b84f68b936
AkBoy7 <akambilbas@gmail.com>
2019-05-23 15:18:04 +0200

    Added Selectable nodes and coloring of the edges when selected

--- backend/static/scripts/ForceLinkDiagramVisClass.js
+++ backend/static/scripts/ForceLinkDiagramVisClass.js
@@ -39,14 +39,16 @@ ForceLink.prototype.setData = function (url) {
                     type: 'webGL'
                 },
                 settings: {
-                    minEdgeSize: 0.01,
+                    minEdgeSize: 0.2,
                     maxEdgeSize: 1,
                     minNodeSize: 2,
                     maxNodeSize: 5,
                     animationTime: 1000,
-                    minArrowSize: 12,
+                    defaultNodeColor: '#0099ff',
+                    minArrowSize: 6,
                     enableHovering: true,
                     doubleClickEnabled: false,
+                    defaultEdgeType: 'arrow'
                 }
             }
         );
@@ -59,7 +61,6 @@ ForceLink.prototype.setData = function (url) {
                 x: P$.random(-2000, 2000),
                 y: P$.random(-1000, 1000),
                 size: P$.random(2, 4),
-                color: '#0099ff'
             });
         }
 
@@ -75,13 +76,14 @@ ForceLink.prototype.setData = function (url) {
                         source: graph.nodes[indexNodes].id,
                         target: graph.nodes[indexEdges].id,
                         color: "#FFFFFF",
-                        type: 'arrow',
                         __proto__: null
                     });
                     i++
                 }
             }
         }
+
+        bindEvents();
 
         // Load the graph in sigma to draw
         s.graph.read(graph);
@@ -97,7 +99,8 @@ ForceLink.prototype.setData = function (url) {
             slowDown: 4,
             barnesHutOptimize: true,
             barnesHutTheta: 0.1,
-            nodeSize: 'original'
+            nodeSize: 'original',
+            autoStop: true
         };
         s.configForceAtlas2(forceAtlas2Config);
 
@@ -105,6 +108,85 @@ ForceLink.prototype.setData = function (url) {
         s.startForceAtlas2(forceAtlas2Config);
         window.setTimeout(function () {
             s.killForceAtlas2();
-        }, 8000);
+        }, 12000);
     };
+
+    function bindEvents() {
+        let nodeId = "0";
+        let oldNode;
+        // Bind the events:
+        s.bind('overNode outNode rightClickNode', function (e) {
+            console.log(e.type, e.data.node.label, e.data.captor);
+        });
+        s.bind('clickNode', function (e) {
+            //colors the edges when clicked on a node
+            s.graph.adjacentEdges(nodeId).forEach(
+                function (ee) {
+                    ee.color = "#FFFFFF";
+                }
+            );
+            console.log(e.type, e.data.node.label, e.data.captor);
+            nodeId = e.data.node.id;
+            s.graph.adjacentEdges(nodeId).forEach(
+                function (ee) {
+                    if (ee.color === '#ff9900' && ee.source === nodeId){
+                        ee.color = "#FFFFFF";
+                    }
+                    else if (ee.source === nodeId){
+                        ee.color = '#ff9900';
+                    }
+                }
+            );
+
+            //colors the selected node orange
+            if (oldNode != null) {
+                oldNode.color = "#0099ff";
+            }
+
+            if (e.data.node.isSelected) {
+                e.data.node.color = "#0099ff";
+                e.data.node.isSelected = false;
+            } else {
+                e.data.node.color = "#ff9900";
+                oldNode = e.data.node;
+                e.data.node.isSelected = true;
+            }
+            s.refresh();
+        });
+        s.bind('doubleClickNode', function (e) {
+            //show the neighbours of the node double clicked on
+            console.log(e.type, e.data.node.label, e.data.captor, e.data.node.id);
+            s.killForceAtlas2();
+            let filter = new sigma.plugins.filter(s);
+            filter.neighborsOf(e.data.node.id);
+            filter.apply();
+            filter.undo();
+            s.refresh();
+        });
+        s.bind('overEdge outEdge clickEdge doubleClickEdge rightClickEdge', function (e) {
+            console.log(e.type, e.data.edge, e.data.captor);
+        });
+        s.bind('clickStage', function (e) {
+            console.log(e.type, e.data.captor);
+        });
+        s.bind('doubleClickStage rightClickStage', function (e) {
+            console.log(e.type, e.data.captor);
+        });
+    }
 }
+
+//Method for finding the adjacent edges return them in an array
+sigma.classes.graph.addMethod('adjacentEdges', function(id) {
+    if (typeof id !== 'string')
+        throw 'adjacentEdges: the node id must be a string.';
+    let a = this.allNeighborsIndex[id],
+        eid,
+        target,
+        edges = [];
+    for(target in a) {
+        for(eid in a[target]) {
+            edges.push(a[target][eid]);
+        }
+    }
+    return edges;
+});
